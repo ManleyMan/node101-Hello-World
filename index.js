048618b42fcceef9a245a1afaacd9b20a57c67f3
ManleyMan <sweezymac@gmail.com>
2022-06-29 11:44:58 -0700

    create a listener using node

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code here
+const http = require('http');
+
+const server = http.createServer((req,res) =>{
+    console.log('Recieved' + req.method + 'request for' + req.url);
+
+//send headers
+    res.writeHead(200, {"Content-Type" : 'text/plain'});
+
+// send body response
+    res.end("Hello World");
+});
+
+// add listener
+server.listen(8080,'localhost', null, function(){
+    console.log("server is listening on localhost:8080");
+});
